4ac7fe721b14bf601c9dea747323b662499538de
alezhuq <okutryk@egmail.com>
2023-01-29 21:57:29 +0200

    chinazez

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = 'мій сайт'
+admin.site.site_header = 'чіназез'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
